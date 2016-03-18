7c1208605a81c71a956d6529980b19c955ce8f6f
mkgramlow <mariska.gramlow@eagles.ewu.edu>
2016-03-18 13:13:04 -0700

    rotate

--- js/index.js
+++ js/index.js
@@ -1,16 +1,3 @@
-/* jquery.js */
-/* jquery.velocity.js */
-/* velocity.ui.js */
-/* jquery.blast.js */
+$('.mariska').lettering();
 
-$(".mariska")
-  // Blast the text apart by word.
-  .blast({ delimiter: "character" })
-  // use "word" to make it looks faster
-  // Fade the words into view using Velocity.js.
-  .velocity("transition.fadeIn", { 
-    display: null,
-    duration: 1250,
-    stagger: 40,
-    delay: 400
-  });
+$('.mariska').lettering('character').fadeOut().fadeIn();
